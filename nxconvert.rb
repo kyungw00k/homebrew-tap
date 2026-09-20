6acc4ddccf49625f3c67e4a3e6ed94aa1a8f2733
Kyungwook Park <parksama@gmail.com>
2026-09-21 07:40:46 +0900

    fix: nxconvert 1.2.0 sha256

--- nxconvert.rb
+++ nxconvert.rb
@@ -9,7 +9,7 @@ class Nxconvert < Formula
   on_macos do
     on_intel do
       url "https://github.com/kyungw00k/nxconvert/releases/download/v1.2.0/nxconvert_darwin_amd64.tar.gz"
-      sha256 "f6e4339d454a660069e2e24d1dd6559691e76155df15558cf6c0cb7863793b65"
+      sha256 "31b0ca9c757e456a6b90c220f7aa5f1a1f651783e72afd02f63f900f71b00e21"
 
       def install
         bin.install "nxconvert"
@@ -17,7 +17,7 @@ class Nxconvert < Formula
     end
     on_arm do
       url "https://github.com/kyungw00k/nxconvert/releases/download/v1.2.0/nxconvert_darwin_arm64.tar.gz"
-      sha256 "879dcc8076547be7aa6ba01558fcece7c9ca4f1085adcf499ad3af14973a4d8d"
+      sha256 "62c7406c0970ca0aead62e4997a3a703631387da9b2d622cbf392e341e01d1bc"
 
       def install
         bin.install "nxconvert"
@@ -28,7 +28,7 @@ class Nxconvert < Formula
   on_linux do
     on_intel do
       url "https://github.com/kyungw00k/nxconvert/releases/download/v1.2.0/nxconvert_linux_amd64.tar.gz"
-      sha256 "e20d6f711b42d0d088e0ee06544132b737aabceca40c00b8798ccdf3d7504c3a"
+      sha256 "12455de6afbac7e09c13c8777756055bfb7b188ece8e3c9a6fa930bc2040939d"
 
       def install
         bin.install "nxconvert"
@@ -36,7 +36,7 @@ class Nxconvert < Formula
     end
     on_arm do
       url "https://github.com/kyungw00k/nxconvert/releases/download/v1.2.0/nxconvert_linux_arm64.tar.gz"
-      sha256 "90e8ac67ac98419f4ba01a36819afd29f0887e26709a61f05b2009d050812279"
+      sha256 "e4e490d32fef860764bd614ff9c536e78a621f07ee483fe17709702c36eadaf6"
 
       def install
         bin.install "nxconvert"
