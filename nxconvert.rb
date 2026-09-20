0efcabf4e135e8ed08ed778a498754d6bf3430d6
Kyungwook Park <parksama@gmail.com>
2026-09-21 07:39:15 +0900

    nxconvert 1.2.0

--- nxconvert.rb
+++ nxconvert.rb
@@ -4,11 +4,11 @@
 class Nxconvert < Formula
   desc "Nintendo Switch NSP/XCI container converter (lossless repack, optional distribution rewrite)"
   homepage "https://github.com/kyungw00k/nxconvert"
-  version "1.0.0"
+  version "1.2.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/kyungw00k/nxconvert/releases/download/v1.0.0/nxconvert_darwin_amd64.tar.gz"
+      url "https://github.com/kyungw00k/nxconvert/releases/download/v1.2.0/nxconvert_darwin_amd64.tar.gz"
       sha256 "f6e4339d454a660069e2e24d1dd6559691e76155df15558cf6c0cb7863793b65"
 
       def install
@@ -16,7 +16,7 @@ class Nxconvert < Formula
       end
     end
     on_arm do
-      url "https://github.com/kyungw00k/nxconvert/releases/download/v1.0.0/nxconvert_darwin_arm64.tar.gz"
+      url "https://github.com/kyungw00k/nxconvert/releases/download/v1.2.0/nxconvert_darwin_arm64.tar.gz"
       sha256 "879dcc8076547be7aa6ba01558fcece7c9ca4f1085adcf499ad3af14973a4d8d"
 
       def install
@@ -27,7 +27,7 @@ class Nxconvert < Formula
 
   on_linux do
     on_intel do
-      url "https://github.com/kyungw00k/nxconvert/releases/download/v1.0.0/nxconvert_linux_amd64.tar.gz"
+      url "https://github.com/kyungw00k/nxconvert/releases/download/v1.2.0/nxconvert_linux_amd64.tar.gz"
       sha256 "e20d6f711b42d0d088e0ee06544132b737aabceca40c00b8798ccdf3d7504c3a"
 
       def install
@@ -35,7 +35,7 @@ class Nxconvert < Formula
       end
     end
     on_arm do
-      url "https://github.com/kyungw00k/nxconvert/releases/download/v1.0.0/nxconvert_linux_arm64.tar.gz"
+      url "https://github.com/kyungw00k/nxconvert/releases/download/v1.2.0/nxconvert_linux_arm64.tar.gz"
       sha256 "90e8ac67ac98419f4ba01a36819afd29f0887e26709a61f05b2009d050812279"
 
       def install
